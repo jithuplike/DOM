df696da25d8bf6185ee7415cdc327118ab38cef8
Jithu p like <jithuplike96@gmail.com>
2024-10-07 12:49:08 -0700

    dom

---
+++ script.js
@@ -0,0 +1,15 @@
+let h2 = document.querySelector("h2")
+h2.innerText="MY QUOTESSS"
+
+ 
+
+let info_box = document.getElementsByClassName("info-box")
+info_box[0].style.color="red"
+info_box[1]. style.color="red"
+info_box[2]. style.color="red"
+
+ let Task = document.getElementsByTagName("li")
+ Task[2].innerHTML = "Task Null"
+
+ let footer = document.querySelector("footer span")
+ footer.innerHTML = " Have a great day Powered by Student,s Webpage"
